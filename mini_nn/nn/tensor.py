ca1e7420e716c737bc2c3c16db333079229beb69
Ashish Sharma <Ashishs.r.s.01@gmail.com>
2026-05-13 22:37:20 +0530

    Add Tensor operations and test script for basic functionality

--- mini_nn/nn/tensor.py
+++ mini_nn/nn/tensor.py
@@ -4,12 +4,18 @@ class Tensor:
 
     def __init__(self,data):
         self.data = np.array(data)
+    
+    def __repr__(self):
+        return f"Tensor({self.data})"
 
     def show(self):
         print(self.data)
 
     def __add__(self,other):
         return Tensor(self.data + other.data)
+
+    def __mul__(self,other):
+        return Tensor(self.data * other.data)
 
     def shape(self):
         return (self.data.shape)
---
+++ mini_nn/nn/test.py
@@ -0,0 +1,22 @@
+from tensor import Tensor
+
+
+a = Tensor([1, 2, 3])
+b = Tensor([4, 5, 6])
+
+print(a)
+print(b)
+
+print(a + b)
+
+print(a * b)
+
+print(a.shape())
+
+m = Tensor([
+    [1, 2],
+    [3, 4]
+])
+
+print(m)
+print(m.shape())
