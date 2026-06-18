7f60763a60fa96f3e68d793e38c33a924c880da8
Ashish Sharma <Ashishs.r.s.01@gmail.com>
2026-06-18 15:52:04 +0530

    Refactor training loop to handle multiple input-output pairs and accumulate total loss

--- mini_nn/nn/train.py
+++ mini_nn/nn/train.py
@@ -1,3 +1,4 @@
+from tensor import Tensor
 from mlp import MLP
 from optimizer import SGD
 
@@ -5,17 +6,41 @@ model = MLP(1, [1])
 
 optimizer = SGD(model.parameters(), lr = 0.01)
 
-x = [2.0]
-target = 10.0
+xs = [
+    [1.0],
+    [2.0],
+    [3.0],
+    [4.0]]
+ys = [
+    2.0,
+    4.0,
+    6.0,
+    8.0
+]
 
-for epoch in range(100):
 
-    pred = model(x)
-    loss = (pred - target)**2
+for epoch in range(200):
+
+    total_loss = None
 
     optimizer.zero_grad()
-    loss.backward()
+
+    for x, y_true in zip(xs, ys):
+
+        pred = model(x)
+
+        loss = (pred - y_true) ** 2
+
+        if total_loss is None:
+            total_loss = loss
+        else:
+            total_loss += loss
+
+    total_loss.backward()
 
     optimizer.step()
 
-    print(f"Epoch {epoch}: Loss = {loss.data}")
+    if epoch % 10 == 0:
+        print(epoch, total_loss.data)
+
+
